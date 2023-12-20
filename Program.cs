1f195609808f127b53c9184bb70e33d6c860206d
unknown <van.tancheg@gmail.com>
2023-12-20 02:01:05 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using aspnet3.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CarServiceContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<CarServiceContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStatusCodePages();
+
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
+
+app.Run();
